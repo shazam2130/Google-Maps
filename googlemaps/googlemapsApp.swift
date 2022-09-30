a152097a192f6bce61ed2b3b40683154d13a2887
shazam2130 <shusingh@student.unimelb.edu.au>
2022-10-01 00:38:38 +1000

    Initial Commit

---
+++ googlemaps/googlemapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  googlemapsApp.swift
+//  googlemaps
+//
+//  Created by Shubham Singh on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct googlemapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
